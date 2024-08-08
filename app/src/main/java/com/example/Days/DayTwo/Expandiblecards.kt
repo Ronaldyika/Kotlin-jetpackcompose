092d6f7f5f246fa7a8d259e8ebbfe6dda150f094
Ronaldyika <buhnyuyronald@gmail.com>
2024-08-08 14:11:15 -0700

    started my card expansion project

---
+++ app/src/main/java/com/example/Days/DayTwo/Expandiblecards.kt
@@ -0,0 +1,15 @@
+package com.example.Days.DayTwo
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ExpandibleCard(){
+
+}
+
+@Preview
+@Composable
+fun displayCard(){
+
+}
